b41b26e8c6b9b652e828635a9b9416d9a67e97b9
Ranieri Lucas <ranieri.dantas.122@ufrn.edu.br>
2023-05-09 11:45:57 -0300

    add: form transactions type controll

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,94 +1,111 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { 
-  TransactionTypeContainer, 
+import {
+  TransactionTypeContainer,
   TransactionTypeButton,
-  ModalOverlay, 
-  ModalContent, 
-  CloseButton, 
-  ModalTitle, 
-  ModalForm, 
+  ModalOverlay,
+  ModalContent,
+  CloseButton,
+  ModalTitle,
+  ModalForm,
 } from "./styles";
 import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import { z } from "zod";
 
 const newTransactionFormSchema = z.object({
   description: z.string(),
   price: z.number(),
   category: z.string(),
-  // type: z.enum(["income", "outcome"])
+  type: z.enum(["income", "outcome"])
 });
 
 type newTransactionFormInputs = z.infer<typeof newTransactionFormSchema>;
 
-function NewTransactionModal(){
+function NewTransactionModal() {
 
-  const { 
+  const {
+    control,
     register,
     handleSubmit,
     formState: { isSubmitting }
   } = useForm<newTransactionFormInputs>({
-    resolver: zodResolver(newTransactionFormSchema)
+    resolver: zodResolver(newTransactionFormSchema),
+    defaultValues: {
+      type: "income"
+    }
   });
 
-  async function handleCreateNewTransaction(data: newTransactionFormInputs){
+  async function handleCreateNewTransaction(data: newTransactionFormInputs) {
     await new Promise(resolve => setTimeout(resolve, 2000));
     console.log(data);
   }
 
   return (
     <Dialog.Portal>
-      <ModalOverlay/>
+      <ModalOverlay />
       <ModalContent>
         <ModalTitle>Nova Transação</ModalTitle>
         <CloseButton>
-          <X size={24}/>
+          <X size={24} />
         </CloseButton>
 
         <ModalForm onSubmit={handleSubmit(handleCreateNewTransaction)}>
-          <input 
-            type="text" 
-            placeholder="Descrição" 
+          <input
+            type="text"
+            placeholder="Descrição"
             required
             {...register("description")}
           />
-          <input 
-            type="text" 
-            placeholder="Preço" 
+          <input
+            type="text"
+            placeholder="Preço"
             required
             {...register("price", { valueAsNumber: true })}
           />
-          <input 
-            type="text" 
-            placeholder="Categoria" 
+          <input
+            type="text"
+            placeholder="Categoria"
             required
             {...register("category")}
           />
 
-          <TransactionTypeContainer>
-            <TransactionTypeButton 
-              variant="income"
-              value="income"
-            >
-              <ArrowCircleUp size={24}/>
-              Entrada
-            </TransactionTypeButton>
-            <TransactionTypeButton
-              variant="outcome"
-              value="outcome"
-            >
-              <ArrowCircleDown size={24}/>
-              Saída
-            </TransactionTypeButton>
-          </TransactionTypeContainer>
+          <Controller
+            name="type"
+            control={control}
+            render={({ field }) => {
+              return (
+                <TransactionTypeContainer
+                  onChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <TransactionTypeButton
+                    variant="income"
+                    value="income"
+                  >
+                    <ArrowCircleUp size={24} />
+                    Entrada
+                  </TransactionTypeButton>
+                  <TransactionTypeButton
+                    variant="outcome"
+                    value="outcome"
+                  >
+                    <ArrowCircleDown size={24} />
+                    Saída
+                  </TransactionTypeButton>
+                </TransactionTypeContainer>
+              );
+            }}
+          />
+
+
 
           <button type="submit" disabled={isSubmitting}>
             Cadastrar
           </button>
         </ModalForm>
 
-        
+
       </ModalContent>
     </Dialog.Portal>
   );
